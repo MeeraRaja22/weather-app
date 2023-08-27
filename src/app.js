57e952661be4a70bb8d91df871a411426b935751
MeeraRaja22 <141357917+MeeraRaja22@users.noreply.github.com>
2023-08-27 21:52:02 +0100

    inserted api data into innerHTML

--- src/app.js
+++ src/app.js
@@ -31,7 +31,6 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
@@ -60,7 +59,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "e7e3dee0435cf0503da944c958447fb4";
+  let apiKey = "3980a7c8f2a782241a093131b099f993";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -91,7 +90,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "e7e3dee0435cf0503da944c958447fb4";
+  let apiKey = "3980a7c8f2a782241a093131b099f993";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -118,7 +117,6 @@ function showCelciusTemperature(event) {
 }
 
 search("London");
-displayForecast();
 
 let celciusTemperature = null;
 
