aaf7b733d31a42fbd406dfdd38caf5ea1364cabd
MeeraRaja22 <141357917+MeeraRaja22@users.noreply.github.com>
2023-08-27 16:20:26 +0100

    moved html to js to create multi-day forecast

--- src/app.js
+++ src/app.js
@@ -18,6 +18,29 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sun", "Mon", "Tues", "Wed"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+    <div class="weather-forecast-date">${day}</div>
+    <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="" width="42" />
+    <div class="weather-forecast-temperature">
+      <span class="weather-forecast-temperature-max">18</span>°
+      <span class="weather-forecast-temperature-min">12</span>°
+    </div>
+  </div>
+`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -69,6 +92,7 @@ function showCelciusTemperature(event) {
 }
 
 search("London");
+displayForecast();
 
 let celciusTemperature = null;
 
